b89af4b4d05adea344fc6ec7d11ad0dfffabc892
kol845 <pebo-hamza@hotmail.com>
2021-12-30 14:33:58 +0700

    Add persistence error catch for unimplemented browsers

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -25,7 +25,19 @@ export default function initFirebase() {
             }
         }
         const db = firebase.firestore();
-        db.enablePersistence();
+        db.enablePersistence().catch(function(err) {
+          if (err.code == 'failed-precondition') {
+              // Multiple tabs open, persistence can only be enabled
+              // in one tab at a a time.
+              // ...
+              console.log(err.code);
+          } else if (err.code == 'unimplemented') {
+              // The current browser does not support all of the
+              // features required to enable persistence
+              // ...
+              console.log(err.code);
+          }
+      });
         console.log('Firebase was successfully init.')
     }
 }
