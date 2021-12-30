342e2d305ae58d30b77f1cdb09afdfe1b11cdf1c
kol845 <pebo-hamza@hotmail.com>
2021-12-30 14:39:35 +0700

    Move firebase folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import { Wrapper, Header, Main, Footer, Cards } from "@components";
 import GlobalStyle from "@styles/globalStyles";
 
-import firebase from '../firebase/initFirebase'
+import firebase from '../src/firebase/initFirebase'
 firebase()
 
 // import firebaseWrite from '../firebase/scripts/write_dummy'
 // firebaseWrite()
-import firebaseRead from '../firebase/scripts/read_dummy'
+import firebaseRead from '../src/firebase/scripts/read_dummy'
 firebaseRead()
 
 const Home: React.FC = () => {
