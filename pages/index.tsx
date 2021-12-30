8c3a63f1e8ee478cef13fe9140471934146b6d68
kol845 <pebo-hamza@hotmail.com>
2021-12-30 12:25:02 +0700

    Add firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Wrapper, Header, Main, Footer, Cards } from "@components";
 import GlobalStyle from "@styles/globalStyles";
 
+import firebase from '../firebase/initFirebase'
+firebase()
+
 const Home: React.FC = () => {
   return (
     <Wrapper>
